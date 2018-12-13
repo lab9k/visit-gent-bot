633d53d290a16788facda792740d78ac3df7451f
Jefwillems <jefwillems100@gmail.com>
2018-12-13 09:55:33 +0100

    refactor codebase to use dialogflow-fulfillment

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,26 @@
 const express = require('express');
-const axios = require('axios');
-// const getData = require('../services/index');
 
 const router = express.Router();
+const { WebhookClient } = require('dialogflow-fulfillment');
+const log = require('../util/log')(__filename.split('/').pop());
 
-function sendMessage(recipientId, message) {
-  axios
-    .post('https://graph.facebook.com/v3.0/me/messages', {
-      params: {
-        access_token: process.env.FB_ACCESS_TOKEN,
-      },
+/**
+ * Map that maps intent names to handlers
+ * @type {Map<string, function>}
+ */
+const intentMap = new Map();
+intentMap.set('Default Welcome Intent', agent => agent.add('welcome!'));
 
-      method: 'POST',
-      data: {
-        recipient: { id: recipientId },
-        message,
-      },
-    })
-    .then((error, response, body) => {
-      if (error) {
-        console.log('Error sending message: ', error);
-      } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-      }
-      console.log(JSON.stringify(body));
-    })
-    .catch(err => console.log(err));
-}
-
+/**
+ * Routes HTTP POST requests to index. It catches all fulfillment's from Dialogflow.
+ */
 router.post('/', (req, res) => {
-  const events = req.body.entry[0].messaging;
-  for (let i = 0; i < events.length; i += 1) {
-    const event = events[i];
-    if (event.message && event.message.text) {
-      sendMessage(event.sender.id, { text: `Echo: ${event.message.text}` });
-    }
+  if (!req.body || Object.keys(req.body).length === 0) {
+    res.status(400).send('Empty body');
   }
-  res.sendStatus(200);
 
-  // getData('combined')((err, data) => (!err ? res.json(data) : res.json({ error: err.message })));
-});
-
-router.get('/', (req, res) => {
-  const mode = req.query['hub.mode'];
-  const challenge = req.query['hub.challenge'];
-  const verifyToken = req.query['hub.verify_token'];
-  if (verifyToken === process.env.FB_VER_VAR) {
-    res.send(challenge);
-  } else {
-    res.send(mode);
-  }
+  const agent = new WebhookClient({ request: req, response: res });
+  agent.handleRequest(intentMap).catch(err => log.log('error', err));
 });
 
 module.exports = router;
