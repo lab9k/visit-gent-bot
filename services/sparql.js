8cff8987ad8626c093abd01dfbeacfac8e7664a4
Jefwillems <jefwillems100@gmail.com>
2018-12-13 11:13:44 +0100

    Promise hotfix

--- services/sparql.js
+++ services/sparql.js
@@ -12,12 +12,12 @@ class SparqlAPI {
 
   getAttractions() {
     const query = 'PREFIX schema: <http://schema.org/> PREFIX n3: <http://schema.org/> PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT DISTINCT ?attraction ?name ?description ?page (GROUP_CONCAT(?image; SEPARATOR=", ") AS ?images) WHERE { ?attraction a <http://schema.org/TouristAttraction> . ?attraction n3:name ?name . ?attraction n3:description ?description . ?attraction foaf:page ?page . ?attraction n3:image ?image . FILTER (langMatches(lang(?name), lang(?description))) . } GROUP BY ?attraction ?name ?description ?page';
-    return rawParse(this.query(query));
+    return this.query(query).then(data => rawParse(data));
   }
 
   getEvents() {
     const query = 'PREFIX schema: <http://schema.org/> PREFIX n3: <http://schema.org/> PREFIX foaf: <http://xmlns.com/foaf/0.1/> SELECT ?attraction ?name ?description ?page (GROUP_CONCAT(?image; SEPARATOR=", ") AS ?images) FROM <http://stad.gent/tourism/events/> WHERE { ?attraction a <http://schema.org/Event> . ?attraction n3:name ?name . ?attraction n3:description ?description . ?attraction foaf:page ?page . ?attraction n3:image ?image . FILTER (langMatches(lang(?name), lang(?description))) . } GROUP BY ?attraction ?name ?description ?page';
-    return rawParse(this.query(query));
+    return this.query(query).then(data => rawParse(data));
   }
 
   query(query) {
