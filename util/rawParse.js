54e16fe9920c207fcf3a354a3314a0bb816ce17d
Jefwillems <jefwillems100@gmail.com>
2018-12-18 09:40:58 +0100

    refactor data parser for more abstract working

--- util/rawParse.js
+++ util/rawParse.js
@@ -1,25 +1,22 @@
-const { map, filter } = require('lodash');
+const {
+  map, filter, pick, mapValues,
+} = require('lodash');
 
 const onlyNl = item => item.name['xml:lang'] === 'nl';
 
 module.exports = (rawData) => {
   const {
+    head: { vars: properties },
     results: { bindings: items },
   } = rawData;
-  return map(
-    filter(items, onlyNl),
-    ({
-      attraction: { value: attraction },
-      name: { value: name },
-      description: { value: description },
-      page: { value: pageUrl },
-      images: { value: imageString },
-    }) => ({
-      attraction,
-      name,
-      description,
-      pageUrl,
-      images: imageString.split(', '),
-    }),
-  );
+  return map(filter(items, onlyNl), (item) => {
+    const picked = pick(item, properties);
+    return mapValues(picked, (v, key) => {
+      const { value } = v;
+      if (key.includes('List')) {
+        return value.split(', ');
+      }
+      return value;
+    });
+  });
 };
